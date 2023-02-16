445c38cd278ea909a6854f4539f1c07b1ab78d22
AlexKuro <115189188+AlexKuro@users.noreply.github.com>
2023-02-17 02:00:30 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,21 +1,6 @@
 """ 
 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. 
 """
-
-f1 = open('file_task_4_1.txt', 'r')
-st1 = f1.readline()
-f1.close()
-
-print(f"\nТекст из файла - '{st1}'\n")
-st2 = ''
-st3 = ''
-st4 = ''
-t = len(st1)
-d = j = 0
-coun = 0
-fl = True
-
-
 def encoder(st1):
     st2 = ''
     st3 = ''
@@ -29,25 +14,28 @@ def encoder(st1):
         if st == st2:
             coun += 1
             st3 = str(coun) + st2
-
     st4 += st3
     return st4
 
 
-st2 = ''
-st3 = ''
-st4 = ''
-for st in st1:
-    if st != st2:
-        coun = 0
-        st4 += st3
-        st2 = st
-
-    if st == st2:
-        coun += 1
-        st3 = str(coun) + st2
-
-st4 += st3
+def decoding(st2):
+    coun = ''
+    st3 = ''
+    for st in st2:
+        if st.isdigit():
+            coun += st
+        else:
+            st3 += st * int(coun)
+            coun = ''
+    return st3
 
 
-print(encoder(st1))
+f1 = open('file_task_4_1.txt', 'r')
+st1 = f1.readline()
+f1.close()
+
+print(f"\nТекст из файла --------> {st1}")
+encod = encoder(st1)
+print(f"\nСжатие данных ---------> {encod}")
+decod = decoding(encod)
+print(f"\nВосстановления данных -> {decod}\n")
